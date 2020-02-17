3dfcdc16b7da72453bb7416bd5fc5e49b9996352
skaftisveins <skaftisveins@gmail.com>
2020-02-17 02:01:38 +0000

    Added Card and ListTile. Added SizedBox and Divider. Linear Gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -10,75 +10,75 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('images/me.png'),
-                radius: 50.0,
-              ),
-              Text(
-                'Skafti Sveinsson',
-                style: GoogleFonts.pacifico(
-                  textStyle: TextStyle(
-                      fontSize: 40.0,
-                      // fontWeight: FontWeight.bold,
-                      color: Colors.white),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.repeated,
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.blue, Colors.black]),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/me.png'),
+                  radius: 50.0,
                 ),
-              ),
-              Text(
-                'Front End Developer',
-                style: GoogleFonts.sourceSansPro(
-                  textStyle: TextStyle(
-                      fontSize: 20.0, letterSpacing: 1.5, color: Colors.white),
+                Text(
+                  'Skafti Sveinsson',
+                  style: GoogleFonts.pacifico(
+                    textStyle: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.phone, color: Colors.teal),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
+                Text(
+                  'Front End Developer',
+                  style: GoogleFonts.sourceSansPro(
+                    textStyle: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 1.5,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 160.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.black),
+                    title: Text(
                       '+354 848 5184',
                       style: GoogleFonts.sourceSansPro(
-                        textStyle: TextStyle(
-                            color: Colors.teal.shade900, fontSize: 20.0),
+                        textStyle:
+                            TextStyle(color: Colors.grey[900], fontSize: 20.0),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.email, color: Colors.teal),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.black),
+                    title: Text(
                       'skaftisveins@gmail.com',
                       style: GoogleFonts.sourceSansPro(
-                        textStyle: TextStyle(
-                            color: Colors.teal.shade900, fontSize: 20.0),
+                        textStyle:
+                            TextStyle(color: Colors.grey[900], fontSize: 20.0),
                       ),
                     ),
-                  ],
-                ),
-              )
-            ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
